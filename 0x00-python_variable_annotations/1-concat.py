eb5a6197f4f9bda38615bba343fd342555dd60ab
emmanuel <emmanuellawakin2005@gmail.com>
2024-01-27 18:40:27 +0100

    Completed task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module takes a string 'str1' and a string 'str2' as arguments
+and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function takes a string 'str1' and a string 'str2' as
+    arguments and returns a concatenated string"""
+    return str1 + str2
