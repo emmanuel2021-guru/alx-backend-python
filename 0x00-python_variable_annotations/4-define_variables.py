f25c1fb9a8a4bbcf9b6c76dadc932e517f802269
emmanuel <emmanuellawakin2005@gmail.com>
2024-01-28 19:28:15 +0100

    Completed task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module defines and annotates some variables with their
+specified values"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
