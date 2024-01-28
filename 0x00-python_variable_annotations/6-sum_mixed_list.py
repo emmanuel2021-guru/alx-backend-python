cc56d0731f066b71ca961fcf28e7e7f3b6cd0078
emmanuel <emmanuellawakin2005@gmail.com>
2024-01-28 20:36:32 +0100

    Fixed pycodestyle error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """This module contains a type-annotated function 'sum_mixed_list' which
-takes a list 'mxd_lst' of integers and floats and returns their sum as a float"""
+takes a list 'mxd_lst' of integers and floats and returns their sum
+as a float"""
 
 from typing import Union, List
 
