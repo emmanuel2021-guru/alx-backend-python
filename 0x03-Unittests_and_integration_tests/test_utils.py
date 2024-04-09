c3dfc5a107705ad4f816337f66abd076bfce766a
emmanuel <emmanuellawakin2005@gmail.com>
2024-04-09 16:32:29 +0100

    completed task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,5 +24,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test Method
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 # if __name__ == "__main__":
 #     unittest.main()
